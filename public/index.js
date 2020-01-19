9fc54b1f33923b894fbb81e13af4d302f62c0cdb
Hadi <hadizzo@localhost.localdomain>
2020-01-19 11:10:24 +0330

    my first commit

---
+++ public/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import React-DOM from 'react-dom';
+const name = 'hadi masoomi';
+const element = <h1>hello  ,{name}</h1>;
+ReactDOM.render(element,
+	document.getElementById('root'));
---
+++ public/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import React-DOM from 'react-dom';
+const name = 'hadi masoomi';
+const Element = () => <h1>hello  ,{name}</h1>;
+ReactDOM.render(Element,
+	document.getElementById('root'));
